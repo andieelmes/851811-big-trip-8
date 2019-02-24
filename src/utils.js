778be16cd81b25c3880dab3b6c3c8e10d5e5542e
z.salikhkulova <z.salikhkulova@fun-box.ru>
2019-02-24 14:33:44 +0500

    remove shuffle function :cry: and and lodash.shuffle

--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,8 @@
+import shuffle from 'lodash.shuffle';
+
 export const getRandomInt = (min, max) => Math.floor(Math.random() * Math.floor((max - min) + 1) + min);
 
-const _shuffle = (array) => {
-  const copyArray = array.slice(0);
-  copyArray.forEach((el, i) => {
-    const randomIndex = getRandomInt(i, copyArray.length - 1);
-    [copyArray[randomIndex], copyArray[i]] = [copyArray[i], copyArray[randomIndex]];
-  });
-  return copyArray;
-};
-
-export const getRandomElements = (array, length) => _shuffle(array).slice(0, length);
+export const getRandomElements = (array, length) => shuffle(array).slice(0, length);
 
 export const populateDom = (array, parentElement, render, clear = false) => {
   const fragment = document.createElement(`template`);
