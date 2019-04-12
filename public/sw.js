f08f9f3b5e63f8671e896269a45b0d18b726190e
z.salikhkulova <z.salikhkulova@fun-box.ru>
2019-04-12 13:02:38 +0500

    fix paths

--- public/sw.js
+++ public/sw.js
@@ -2,13 +2,13 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('trip').then(function(cache) {
      return cache.addAll([
-       '/',
-       '/index.html',
-       '/styles/main.css',
-       '/styles/normalize.css',
-       '/bundle.js',
-       '/img/star--check.svg',
-       '/img/star.svg'
+       './',
+       './index.html',
+       './css/main.css',
+       './css/normalize.css',
+       './bundle.js',
+       './img/star--check.svg',
+       './img/star.svg'
      ]);
    }).catch(function(error) {
     console.log(error);
